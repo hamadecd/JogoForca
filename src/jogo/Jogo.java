b4f2a29d480703e45f9500047b4f15de1240e276
Samir <samircd9@gmail.com>
2023-05-12 22:11:53 -0300

    Tratando da lógica do jogo

--- src/jogo/Jogo.java
+++ src/jogo/Jogo.java
@@ -1,31 +1,44 @@
 package jogo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Jogo {
     public static void main(String[] args) {
-        String palavra = "farol";
-        String tentativa;
-        int chances = 1;
-        int acertos = palavra.length();
+        String palavra = "arara";
+        Character tentativa;
+        int chances = 6;
+        List<String> letrasJogadas = new ArrayList();
         Scanner input = new Scanner(System.in);
 
-        while (chances <= 6) {
-            System.out.println("Informe uma letra ou adivinhe a palavra: ");
-            tentativa = input.nextLine();
+        System.out.println("BEM VINDO AO JOGO DA FORCA");
 
-            if (tentativa.equals(palavra)) {
-                System.out.println("Parabéns você acertou!");
+        while (chances >= 1) {
+            System.out.println("Você tem " + chances + " chances!\n");
+            System.out.println("Informe uma letra ou adivinhe a palavra: ");
+            tentativa = input.next().toLowerCase().charAt(0);
+
+            if (letrasJogadas.contains(tentativa.toString())) {
+                System.out.println("Caiu na tratativa");
+                System.out.println(letrasJogadas);
                 break;
-            } else if (palavra.contains(tentativa)) {
-                acertos--;
-                if (acertos == 0) {
+            }
+            System.out.println("você digitou: " + tentativa);
+
+            if (palavra.contains(tentativa.toString())) {
+                palavra = palavra.replaceAll(tentativa.toString(), "");
+                System.out.println("Palavra depois de ter acertado uma letra: " + palavra);
+                if (palavra.isEmpty()) {
                     System.out.println("Parabéns você acertou!");
                     break;
                 }
             } else {
-                chances++;
+                chances--;
             }
+
+            letrasJogadas.add(tentativa.toString());
+            System.out.println(letrasJogadas);
 
         }
 
